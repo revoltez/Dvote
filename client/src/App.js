40314e7ef99c1d8109184f93b3dd747d2ce588c9
Ego_Renegade <eziorevoltez@gmail.com>
2022-08-08 14:15:26 +0100

    Fixed truffle configs

--- client/src/App.js
+++ client/src/App.js
@@ -6,56 +6,62 @@ import "./App.css";
 import { Loader, Group } from "@mantine/core";
 
 function App() {
-  const [web3, setWeb3] = useState(null);
-  const [accounts, setAccounts] = useState([]);
-  const [contractInstance, setContractInstance] = useState(null);
-  const [myAddr, setMyAddr] = useState(null);
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const web3 = await getWeb3();
-        const Accounts = await web3.eth.getAccounts();
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = DvoteContract.networks[networkId];
-        const instance = new web3.eth.Contract(
-          DvoteContract.abi,
-          web3.utils.toChecksumAddress(
-            deployedNetwork && deployedNetwork.address
-          )
-        );
+	const [web3, setWeb3] = useState(null);
+	const [accounts, setAccounts] = useState([]);
+	const [contractInstance, setContractInstance] = useState(null);
+	const [myAddr, setMyAddr] = useState(null);
+	useEffect(() => {
+		const init = async () => {
+			try {
+				const web3 = await getWeb3();
+				const Accounts = await web3.eth.getAccounts();
+				const networkId = await web3.eth.net.getId();
+				const deployedNetwork =
+					DvoteContract.networks[networkId];
+				const instance = new web3.eth.Contract(
+					DvoteContract.abi,
+					deployedNetwork.address
+				);
+				setWeb3(web3);
+				setMyAddr(Accounts[0]);
+				setAccounts(Accounts);
+				setContractInstance(instance);
+			} catch (error) {
+				alert(
+					`Failed to load web3, accounts, or contract. make sure you installed a wallet plugin such as metamask,Check console for details.`
+				);
+				console.error(error);
+			}
+		};
+		init();
 
-        setWeb3(web3);
-        setMyAddr(Accounts[0]);
-        setAccounts(Accounts);
-        setContractInstance(instance);
-      } catch (error) {
-        alert(
-          `Failed to load web3, accounts, or contract. make sure you installed a wallet plugin such as metamask,Check console for details.`
-        );
-        console.error(error);
-      }
-    };
-    init();
+		window.ethereum.on("accountsChanged", async (accounts) => {
+			setAccounts(accounts);
+			setMyAddr(accounts[0]);
+		});
+	}, []);
 
-    window.ethereum.on("accountsChanged", async (accounts) => {
-      setAccounts(accounts);
-      setMyAddr(accounts[0]);
-    });
-  }, []);
-
-  if (!web3) {
-    return (
-      <Group position="center" style={{ height: "100vh" }}>
-        <Loader color="violet" size={50} variant="bars" />
-      </Group>
-    );
-  } else {
-    return (
-      <React.Fragment>
-        <HomeScreen web3={web3} myAddr={myAddr} instance={contractInstance} />
-      </React.Fragment>
-    );
-  }
+	if (!web3) {
+		return (
+			<Group position="center" style={{ height: "100vh" }}>
+				<Loader
+					color="violet"
+					size={50}
+					variant="bars"
+				/>
+			</Group>
+		);
+	} else {
+		return (
+			<React.Fragment>
+				<HomeScreen
+					web3={web3}
+					myAddr={myAddr}
+					instance={contractInstance}
+				/>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,18 @@
 const path = require("path");
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-  networks: {
-    develop: {
-      port: 8545,
-    },
-  },
-  compilers: {
-    solc: {
-      version: "0.8.0",
-    },
-  },
+	// See <http://truffleframework.com/docs/advanced/configuration>
+	// to customize your Truffle configuration!
+	contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+	networks: {
+		development: {
+			host: "127.0.0.1",
+			port: 8545,
+			network_id: "*",
+		},
+	},
+	compilers: {
+		solc: {
+			version: "0.8.0",
+		},
+	},
 };
